c4deebc22e72938f5e6761229c06c08e8dd17127
yeahXD <zltmdhssl@gmail.com>
2016-07-22 17:24:56 +0900

    Day 05

--- bootstrap/client/main.js
+++ bootstrap/client/main.js
@@ -1,24 +1,53 @@
 Template.body.events({
-    'click #btn_write': function (evt, tmpl) {
+    'click #btn_write': function(evt, tmpl) {
         evt.preventDefault();
+
         var name = $("#writer").val();
-        var article = $("#article").val();
+        var article = $('#article').val();
 
         var obj = {};
         obj.name = name;
         obj.article = article;
-        Database.insert(obj);
+        // Database.insert(obj);
+        Meteor.call('insertData', obj, function (err, rslt) {
+            if(err) alert(err);
+            else alert(rslt);
+        });
     },
-    'click #btn_remove': function (evt, tmpl) {
+    'click #btn_remove': function(evt, tmpl) {
         evt.preventDefault();
-        Database.remove({
-            id: this._id
+
+        // Database.remove({
+        //     _id: this._id
+        // });
+        Meteor.call('removeData', this._id, function (err, rslt) {
+            alert(rslt);
+        });
+    },
+    'click #btn_login': function(evt, tmpl) {
+        evt.preventDefault();
+
+        var myid = $("#myid").val();
+        var mypw = $('#mypw').val();
+
+        var obj = {};
+        obj.myid = myid;
+        obj.mypw = mypw;
+        // Database.insert(obj);
+        Meteor.call('login', obj, function (err, rslt) {
+            if(err) alert(err);
+            else alert(rslt);
         });
     }
+
 });
 
 Template.body.helpers({
-   lists: function () {
-       return Database.find().fetch();
-   }
+    lists: function() {
+        // return Database.find().fetch();
+        Meteor.call('readData', function(err, rslt){
+            Session.set('lists', rslt);
+        });
+        return Session.get('lists');
+    }
 });
--- bootstrap/server/methods.js
+++ bootstrap/server/methods.js
@@ -1,33 +1,63 @@
-cl = console.log;
+// cl = console.log;
+//
+// var tmp = Test.find({ no: 2963 }).fetch();
+// var tmp2 = Test.findOne({ no: 3827 });
+// var tmp3 = Test.find({
+//     no: {$gte: 2134, $lt: 2199, $ne: 2157},
+//     calc: {$in: [27134310, 27097275]}
+// }).fetch();
+// var standard = Test.findOne({
+//     _id: 'ADswYCSMSqCxhaycP'
+// });
+//
+// // cl(Test.find({
+// //    createdAt: {$lt: standard.createdAt}
+// // }).fetch().length);
+//
+// // cl(tmp[0]);
+// // cl(tmp2);
+// // cl(tmp3);
+// // cl(standard);
+//
+// var obj = {};
+// obj.key = 'val';
+// obj.key2 = 'val2';
+// obj.key3 = 10;
+// cl(obj);
+//
+// var arr = [];
+// arr.push('val1');
+// arr.push('val2');
+// cl(arr);
+// arr.pop();
+// cl(arr.length);
 
-var tmp = Test.find({ no: 2963 }).fetch();
-var tmp2 = Test.findOne({ no: 3827 });
-var tmp3 = Test.find({
-    no: {$gte: 2134, $lt: 2199, $ne: 2157},
-    calc: {$in: [27134310, 27097275]}
-}).fetch();
-var standard = Test.findOne({
-    _id: 'ADswYCSMSqCxhaycP'
+
+Meteor.methods({
+    'insertData': function (obj) {
+        //로그인되어있는가? 권한이 있는가? --> validation
+        Database.insert(obj);
+        return "글이 작성되었습니다";
+    },
+    'readData': function () {
+        var lists = Database.find().fetch();
+        return lists;
+    },
+    'removeData': function (_id) {
+        Database.remove({_id: _id});
+        return "삭제되었습니다.";
+    },
+    'login': function (obj) {
+        var id = obj.myid;
+        var pw = obj.mypw;
+        var user = Database.findOne({id: id, pw: pw});
+        if(user){
+            if(user.pw === pw){
+                return '로그인되었습니다.'
+            }else
+                return '비밀번호가 다릅니다'
+        }else{
+            return '일치하는 사용자가 없습니다.'
+        }
+    }
 });
-
-// cl(Test.find({
-//    createdAt: {$lt: standard.createdAt}
-// }).fetch().length);
-
-// cl(tmp[0]);
-// cl(tmp2);
-// cl(tmp3);
-// cl(standard);
-
-var obj = {};
-obj.key = 'val';
-obj.key2 = 'val2';
-obj.key3 = 10;
-cl(obj);
-
-var arr = [];
-arr.push('val1');
-arr.push('val2');
-cl(arr);
-arr.pop();
-cl(arr.length);
